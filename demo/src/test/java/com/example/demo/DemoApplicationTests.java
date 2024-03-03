755a4e13a97c2fb270eb6c0597926b3ca97284c0
Lenu-san <110700221+Lenu-san@users.noreply.github.com>
2024-03-03 03:40:55 +0100

    Add files via upload

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,31 +1,13 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.boot.test.context.SpringBootTest;
 
-public class CalculatorTest {
+@SpringBootTest
+class DemoApplicationTests {
 
-    @Test
-    public void testAdd() {
-        Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
-    }
+	@Test
+	void contextLoads() {
+	}
 
-    @Test
-    public void testSubtract() {
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.subtract(5, 3));
-    }
-
-    @Test
-    public void testMultiply() {
-        Calculator calculator = new Calculator();
-        assertEquals(15, calculator.multiply(3, 5));
-    }
-
-    @Test
-    public void testDivide() {
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.divide(6, 3));
-    }
 }
