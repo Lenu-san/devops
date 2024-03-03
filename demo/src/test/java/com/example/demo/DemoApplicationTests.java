668d7b38ba7f8a8e73e96ce04cc69553e6d81490
Lenu-san <110700221+Lenu-san@users.noreply.github.com>
2024-03-03 03:19:08 +0100

    Update DemoApplicationTests.java

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,5 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-
-
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
