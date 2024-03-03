c7992b64428346f30d5e623901134d6211b2821b
Lenu-san <110700221+Lenu-san@users.noreply.github.com>
2024-03-03 03:14:20 +0100

    Update DemoApplicationTests.java

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,4 +4,40 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+public class DemoApplicationTests {
 
+   
+    static class Calculatrice {
+
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        public int subtract(int a, int b) {
+            return a - b;
+        }
+
+        public int multiply(int a, int b) {
+            return a * b;
+        }
+    }
+
+    @Test
+    public void testAddition() {
+        Calculatrice calculatrice = new Calculatrice();
+        assertThat(calculatrice.add(2, 3)).isEqualTo(5);
+    }
+    
+    @Test
+    public void testSubtraction() {
+        Calculatrice calculatrice = new Calculatrice();
+        assertThat(calculatrice.subtract(5, 3)).isEqualTo(2);
+    }
+
+    
+    @Test
+    public void testMultiplication() {
+        Calculatrice calculatrice = new Calculatrice();
+        assertThat(calculatrice.multiply(2, 3)).isEqualTo(6);
+    }
+}
