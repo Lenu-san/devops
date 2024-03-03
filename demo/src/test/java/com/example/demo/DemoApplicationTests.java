bf2fd808d08de7721cbc8c32d333de23eae58ca8
Lenu-san <110700221+Lenu-san@users.noreply.github.com>
2024-03-03 03:18:23 +0100

    Update DemoApplicationTests.java

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -12,19 +13,24 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(Integer.valueOf(5), calculator.add(2, 3));
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.subtract(5, 3));
     }
 
     @Test
     public void testMultiply() {
         Calculator calculator = new Calculator();
-        assertEquals(Integer.valueOf(6), calculator.multiply(2, 3));
+        assertEquals(15, calculator.multiply(3, 5));
     }
 
     @Test
     public void testDivide() {
         Calculator calculator = new Calculator();
-        assertEquals(Integer.valueOf(2), calculator.divide(6, 3));
+        assertEquals(2, calculator.divide(6, 3));
     }
 }
-
